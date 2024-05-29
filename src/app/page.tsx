849fb5ba010286033d2913288b21a89eef8edd4c
Chase-42 <chaseofthecollins@gmail.com>
2024-05-29 11:24:42 -0600

    Scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Link from "next/link";
-
 export default function HomePage() {
+	const mockUrls = [
+		"https://utfs.io/f/c363818d-fd58-4abf-a7a6-0403f67c8166-pet6d2.png",
+		"https://utfs.io/f/b287e342-8b0b-4020-a006-aef540dc98f2-pdme8p.png",
+		"https://utfs.io/f/a755fecc-4455-458e-8ab3-52137395b6db-pe7gt4.png",
+	];
+
+	const mockImages = mockUrls.map((url, index) => ({
+		id: index + 1,
+		url,
+	}));
+
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-			<div>Hello (saving recipes coming soon...)</div>
+		<main className="">
+			<div className="flex flex-wrap">
+				{mockImages.map((image) => (
+					<div key={image.id} className="w-1/2 p-2">
+						<img src={image.url} alt="" />
+					</div>
+				))}
+			</div>
 		</main>
 	);
 }
