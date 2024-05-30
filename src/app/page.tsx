3f788de487fd09e7209dcc493a363dfd0b5541de
Chase-42 <chaseofthecollins@gmail.com>
2024-05-30 11:23:17 -0600

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,28 @@
-export default function HomePage() {
-	const mockUrls = [
-		"https://utfs.io/f/c363818d-fd58-4abf-a7a6-0403f67c8166-pet6d2.png",
-		"https://utfs.io/f/b287e342-8b0b-4020-a006-aef540dc98f2-pdme8p.png",
-		"https://utfs.io/f/a755fecc-4455-458e-8ab3-52137395b6db-pe7gt4.png",
-	];
+import { db } from "~/server/db";
 
-	const mockImages = mockUrls.map((url, index) => ({
-		id: index + 1,
-		url,
-	}));
+const mockUrls = [
+	"https://utfs.io/f/c363818d-fd58-4abf-a7a6-0403f67c8166-pet6d2.png",
+	"https://utfs.io/f/b287e342-8b0b-4020-a006-aef540dc98f2-pdme8p.png",
+	"https://utfs.io/f/a755fecc-4455-458e-8ab3-52137395b6db-pe7gt4.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+	id: index + 1,
+	url,
+}));
+
+export default async function HomePage() {
+	const recipes = await db.query.recipes.findMany();
+	console.log(recipes);
 
 	return (
 		<main className="">
 			<div className="flex flex-wrap">
+				{recipes.map((recipe) => (
+					<div key={recipe.id} className="w-1/2 p-2">
+						{recipe.instructions}
+					</div>
+				))}
 				{mockImages.map((image) => (
 					<div key={image.id} className="w-1/2 p-2">
 						<img src={image.url} alt="" />
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,11 +3,12 @@
 
 import { sql } from "drizzle-orm";
 import {
-  index,
-  pgTableCreator,
-  serial,
-  timestamp,
-  varchar,
+	index,
+	pgTableCreator,
+	serial,
+	text,
+	timestamp,
+	varchar,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -18,17 +19,25 @@ import {
  */
 export const createTable = pgTableCreator((name) => `recipe_vault_${name}`);
 
-export const posts = createTable(
-  "post",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt", { withTimezone: true }),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+export const recipes = createTable("recipes", {
+	id: serial("id").primaryKey(),
+	link: text("link").notNull(),
+	imageUrl: text("imageUrl").notNull(),
+	instructions: text("instructions").notNull(),
+	createdAt: timestamp("createdAt").defaultNow().notNull(),
+});
+
+// export const posts = createTable(
+//   "post",
+//   {
+//     id: serial("id").primaryKey(),
+//     name: varchar("name", { length: 256 }),
+//     createdAt: timestamp("created_at", { withTimezone: true })
+//       .default(sql`CURRENT_TIMESTAMP`)
+//       .notNull(),
+//     updatedAt: timestamp("updatedAt", { withTimezone: true }),
+//   },
+//   (example) => ({
+//     nameIndex: index("name_idx").on(example.name),
+//   })
+// );
