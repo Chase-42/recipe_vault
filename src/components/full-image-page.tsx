984f09049d95cbc60677a824af3249333b9ff536
Chase-42 <chaseofthecollins@gmail.com>
2024-06-20 18:41:49 -0600

    Layout tweaks for full-page and modal.

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -2,5 +2,19 @@ import { getRecipe } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
 	const recipe = await getRecipe(props.id);
-	return <img src={recipe.imageUrl} alt="image" className="w-96" />;
+	return (
+		<div className="flex h-full w-full min-w-0 bg-green-200">
+			<div className="flex-shrink">
+				<img
+					src={recipe.imageUrl}
+					alt="image"
+					className="object-contain flex-shrink"
+				/>
+			</div>
+
+			<div className="w-48 flex flex-col flex-shrink-0">
+				<div className="text-xl font-bold">{recipe.name}</div>
+			</div>
+		</div>
+	);
 }
