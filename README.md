e130afee1d875109e88152484a4a3aee44b4f13a
Chase-42 <chaseofthecollins@gmail.com>
2024-06-24 17:05:50 -0600

    Finalize modal and paralell image routes.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 - [x] "taint" (server-only)
 - [x] Use Next Image component
 - [x] Error management (w/ Sentry)
-- [ ] Routing/recipe page (parallel routes)
+- [x] Routing/recipe page (parallel routes)
 - [ ] Delete button (w/ server actions)
 - [ ] Analytics (posthog)
 - [ ] Ratelimiting (upstash)
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -3,19 +3,20 @@ import { getRecipe } from "~/server/queries";
 export default async function FullPageImageView(props: { id: number }) {
 	const recipe = await getRecipe(props.id);
 	return (
-		<div className="flex h-full w-full min-w-">
-			<div className="flex-shrink flex justify-center items-center">
+		<div className="flex h-full w-full">
+			<div className="w-1/2 flex flex-col flex-shrink-0 border-r p-4">
+				<div className="border-b text-center text-lg p-2">{recipe.name}</div>
+				<div className="border-b text-center text-sm p-2">
+					{recipe.instructions}
+				</div>
+				<div className="border-b text-center text-sm p-2"> {recipe.link}</div>
+			</div>
+			<div className="w-1/2 flex justify-center items-center">
 				<img
 					src={recipe.imageUrl}
 					alt="image"
-					className="object-contain flex-shrink"
+					className="object-contain w-full h-full p-4"
 				/>
-			</div>
-
-			<div className="w-48 flex flex-col flex-shrink-0 border-l">
-				<div className="text-xl font-bold">{recipe.name}</div>
-				<div className="text-sm">{recipe.instructions}</div>
-				<div className="text-sm"> {recipe.link}</div>
 			</div>
 		</div>
 	);
