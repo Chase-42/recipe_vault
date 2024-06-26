1028f8bde99abc8fe2fb2a89b5715f0c361ef2b7
Chase-42 <chaseofthecollins@gmail.com>
2024-06-26 17:15:17 -0600

    Setup shadui.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
 	return (
 		<ClerkProvider>
-			<html lang="en" className={`${GeistSans.variable}`}>
+			<html lang="en" className={`${GeistSans.variable} dark`}>
 				<body>
 					<div className="h-screen grid grid-rows-[auto,1fr]">
 						<TopNav />
