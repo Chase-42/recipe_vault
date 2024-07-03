437c9b07e4f26a334ad5292a8cef8081ebde4496
Chase-42 <chaseofthecollins@gmail.com>
2024-07-03 14:23:46 -0600

    Moved python function to separate api directory.

--- src/api/flask/app.py
+++ src/api/flask/app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 def scraper():
     return jsonify(message="This is the scraper endpoint")
 
-# Ensure that app.py works as a module
 if __name__ != "__main__":
     from werkzeug.middleware.dispatcher import DispatcherMiddleware
     from werkzeug.serving import run_simple
