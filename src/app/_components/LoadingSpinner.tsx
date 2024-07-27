5fd4023585da5580de016823ec37f5b982e0a984
Chase-42 <chaseofthecollins@gmail.com>
2024-07-27 13:34:26 -0600

    Loading.tsx/suspense.

--- src/app/_components/LoadingSpinner.tsx
+++ src/app/_components/LoadingSpinner.tsx
@@ -1,16 +1,8 @@
-import { motion } from "framer-motion";
+import React from "react";
 
 const LoadingSpinner = () => (
   <div className="flex h-full items-center justify-center">
-    <motion.div
-      className="mt-10 h-32 w-32 rounded-full border-b-4 border-t-4 border-red-800 sm:h-48 sm:w-48 md:h-64 md:w-64"
-      animate={{ rotate: 360 }}
-      transition={{
-        repeat: Number.POSITIVE_INFINITY,
-        duration: 2,
-        ease: "linear",
-      }}
-    />
+    <div className="mt-10 h-32 w-32 animate-spin rounded-full border-b-4 border-t-4 border-red-800 sm:h-48 sm:w-48 md:h-64 md:w-64" />
   </div>
 );
 
