492eb82a1ed18f7ca53c25435fb94f5aa6778fc7
Chase-42 <chaseofthecollins@gmail.com>
2024-10-09 16:59:23 -0600

    Rework landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,30 @@ import RecipeList from "~/app/_components/RecipeList";
 
 export default async function HomePage() {
   return (
-    <main className="">
+    <main>
       <SignedOut>
-        <div className="mt-3 h-full w-full text-center text-2xl">
-          Please sign in to view recipes
+        <div className="flex min-h-screen flex-col items-center bg-[hsl(0,0%,3.9%)] p-8 text-center">
+          <div className="animate-fadeIn pb-8 pt-12">
+            <h1 className="mb-4 text-4xl font-bold tracking-wide text-white transition-colors duration-300 hover:text-[hsl(0,72.2%,50.6%)]">
+              Welcome to{" "}
+              <span className="text-[hsl(0,72.2%,50.6%)]">Recipe Vault</span>
+            </h1>
+            <p className="mx-auto max-w-md text-lg text-gray-300">
+              Save all your favorite recipes in one place by sharing a recipe
+              link. We’ll automatically extract the ingredients, instructions,
+              and an image—so you can skip the endless scrolling and focus on
+              cooking.
+            </p>
+          </div>
+
+          <div className="mt-6">
+            <p className="text-xl text-[hsl(0,72.2%,50.6%)] transition-colors duration-300 hover:text-white">
+              Please sign in to start saving recipes
+            </p>
+          </div>
         </div>
       </SignedOut>
+
       <SignedIn>
         <RecipeList />
       </SignedIn>
