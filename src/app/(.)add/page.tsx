0299fd01e1d4ed5de53871ef1bfc2ae6675d42b7
Chase-42 <chaseofthecollins@gmail.com>
2025-04-26 18:43:59 -0600

    fix: follow app's modal pattern for add page

--- src/app/(.)add/page.tsx
+++ src/app/(.)add/page.tsx
@@ -1,21 +1,22 @@
+"use client";
+
 import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
 import AddRecipeForm from "~/app/_components/AddRecipeForm";
 
 const Modal = dynamic(
   () => import("~/app/_components/Modal").then((mod) => mod.Modal),
   {
     ssr: false,
-    loading: () => (
-      <div className="flex h-full items-center justify-center">
-        <div className="h-32 w-32 animate-spin rounded-full border-b-4 border-t-4 border-red-800" />
-      </div>
-    ),
+    loading: () => null,
   }
 );
 
 export default function AddRecipePage() {
+  const router = useRouter();
+
   return (
-    <Modal>
+    <Modal onClose={() => router.back()}>
       <div className="flex h-full w-full flex-col">
         <div className="border-b p-2 text-center text-lg font-bold">
           Add New Recipe
