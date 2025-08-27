1123108db0f1b347749ea44cc01fb0446abcf758
Chase-42 <chaseofthecollins@gmail.com>
2025-08-27 09:18:23 -0600

    Remove unused loading components and update package.json

--- src/app/add/loading.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingSpinner from "~/app/_components/LoadingSpinner";
-
-export default function Loading() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <LoadingSpinner fullHeight={false} />
-    </div>
-  );
-}
--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingSpinner from "~/app/_components/LoadingSpinner";
-
-export default function Loading() {
-  return (
-    <div className="flex h-[calc(100vh-200px)] items-center justify-center">
-      <LoadingSpinner size="lg" />
-    </div>
-  );
-}
